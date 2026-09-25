cd5075810e5bd93f87903634d09f384d4ebbf3db
chavistspeed <chavis@itrainspeed.com>
2026-09-25 15:51:34 -0400

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'iTrainSpeed',description:'Speed & Agility Training'}
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
